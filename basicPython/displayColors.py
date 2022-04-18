417142316384b52f805c59417693921e2b8195ff
priyanka <priyanka.chavan1408@gmail.com>
2022-04-18 17:51:04 +0530

    [Priyanka]Add:Display the first and last element from the list.

---
+++ basicPython/displayColors.py
@@ -0,0 +1,30 @@
+'''
+@Author : Priyanka
+@Date : 2022-04-18  11:00:00
+@Last Modified by : Priyanka
+@Last Modified Time : 2022-04-018 11:10:00
+@Title : Display the first and last item from the list.
+'''
+
+
+def display_list_items():
+    """
+      Description:
+          Access the list items by index and printing the first and last item.
+      Parameter:
+           none
+      Return:
+          Returning nothing but printing the first and last item from th list.
+  """
+
+    color_list = ["Red", "Green", "White", "Black"]
+    x = color_list[0]
+    y = color_list[3]
+    print("First and last element in the list are :",x,y)
+
+display_list_items()
+
+
+
+
+
