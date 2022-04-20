e904485a70989e82fa66883ed0774d2a737ca6ce
priyanka <priyanka.chavan1408@gmail.com>
2022-04-20 18:04:28 +0530

    [Priyanka]Add:Creating tuple with different data type.

---
+++ tuplePrograms/02_tupleWithdifferentDataType.py
@@ -0,0 +1,22 @@
+'''
+@Author : Priyanka
+@Date : 2022-04-20  1:25:00
+@Last Modified by : Priyanka
+@Last Modified Time : 2022-04-020 1:30:00
+@Title :Write a Python program to create a tuple with different data types.
+'''
+
+def tuple_with_different_data_type():
+    """
+       Description:
+            creating a tuple with different data type and print it.
+       Parameter:
+            none
+       Return:
+           Returning string.
+   """
+    thistuple = ("apple", "banana",25, "cherry",12,3.89,False)
+    print("Tuple with different data type is",thistuple)
+
+if __name__ == '__main__':
+    tuple_with_different_data_type()
