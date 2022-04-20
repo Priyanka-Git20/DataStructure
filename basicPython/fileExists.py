4b7ec4c0f1240420b4b309aae182ec988fa83db8
priyanka <priyanka.chavan1408@gmail.com>
2022-04-20 19:23:55 +0530

    [Priyanka]refactor:Using os module instead of pathlib.

--- basicPython/fileExists.py
+++ basicPython/fileExists.py
@@ -1,35 +1,29 @@
-'''
+''
 @Author : Priyanka
 @Date : 2022-04-18  2:00:00
 @Last Modified by : Priyanka
-@Last Modified Time : 2022-04-018 2:15:00
+@Last Modified Time : 2022-04-019 12:15:00
 @Title :Write a Python program to check whether a file exists.
 '''
 
-from pathlib import Path
+
+import os
 
 
 def file_existence():
     """
       Description:
-           Check whether a file exists or not by using file name or path of that file.
+           Check whether a file exists or not by using exists function in the os module.
       Parameter:
            None
       Return:
           Returning nothing but printing the message.
     """
 
-    path_to_file = input("Enter the file or a symlink to a file")
-    path = Path(path_to_file)
+    file_exists = os.path.exists("/String/03_replaceString.py")
+    print(file_exists)
 
-    if path.is_file():
-        print('The file {} exists'.format(path_to_file))
-    else:
-        print('The file {} does not exist'.format(path_to_file))
+if __name__ == '__main__':
 
-file_existence()
-
-
-
-
+    file_existence()
 
